8748b949d377cc08d1a603b169bcff1835c00e2d
shivanividya <shivani.vidyarthi@gmail.com>
2026-03-25 16:49:23 +0000

    commiting calculate.py file for Assignment 1

---
+++ Assignment1/calculate.py
@@ -0,0 +1,25 @@
+#1
+
+n=10
+r=5
+p=100
+
+𝐴 =  𝑝* ((1 + 𝑟/ 100))**n
+
+print(A)
+
+#2
+
+A=10
+B=20
+str = "There are {} students in the class, with {} who play at least one sport."
+
+print(str.format(B,A))
+
+#3
+
+#Sample output = It goes without saying, “Time is Money”, and none can deny it
+
+
+print('It goes without saying, "Time is Money", and none can deny it.')
+
